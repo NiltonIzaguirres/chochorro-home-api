d5b76028bb48cf33984a765f515711caf2db1385
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2024-06-27 14:42:44 -0300

    test: make sure you can't authenticate with wrong email

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -2,6 +2,7 @@ import { beforeEach, describe, expect, it } from 'vitest'
 import { AuthenticateUseCase } from './authenticate'
 import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository'
 import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from './erros/invalid-credentials-error'
 
 let orgsRepository: InMemoryOrgsRepository
 let sut: AuthenticateUseCase
@@ -33,5 +34,23 @@ describe('Authenticate Use Case', async () => {
         password_hash: expect.any(String),
       }),
     )
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    await orgsRepository.create({
+      nameOfPersonResponsible: 'John Doe',
+      email: 'johndoe@example.com',
+      cep: '12345678',
+      password_hash: await hash('password123', 6),
+      address: '123 Main St',
+      phone: '1234567890',
+    })
+
+    await expect(
+      sut.execute({
+        email: 'wrong@example.com',
+        password: 'password123',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 })
