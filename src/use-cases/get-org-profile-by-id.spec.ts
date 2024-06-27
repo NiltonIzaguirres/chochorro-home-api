7b99f4b3f6ddd5fa04f8375171f3b89f53c55232
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2024-06-27 15:48:43 -0300

    test: ensure the return of the org profile with the corret value

---
+++ src/use-cases/get-org-profile-by-id.spec.ts
@@ -0,0 +1,34 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { GetOrgProfileByIdUseCase } from './get-org-profile-by-id'
+import { hash } from 'bcryptjs'
+import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository'
+
+let orgsRepository: InMemoryOrgsRepository
+let sut: GetOrgProfileByIdUseCase
+
+describe('Get org profile by ID use case', async () => {
+  beforeEach(() => {
+    orgsRepository = new InMemoryOrgsRepository()
+    sut = new GetOrgProfileByIdUseCase(orgsRepository)
+  })
+
+  it('should return org profile', async () => {
+    const createdOrg = await orgsRepository.create({
+      nameOfPersonResponsible: 'John Doe',
+      email: 'johndoe@example.com',
+      cep: '12345678',
+      password_hash: await hash('password123', 6),
+      address: '123 Main St',
+      phone: '1234567890',
+    })
+
+    const { org } = await sut.execute({ id: createdOrg.id })
+
+    expect(org).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        createdAt: expect.any(Date),
+      }),
+    )
+  })
+})
