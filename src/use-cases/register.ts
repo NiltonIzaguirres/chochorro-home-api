fc493ab9b62209b08af983263264c452bc9ac2dd
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2024-06-27 12:37:29 -0300

    feat: ensure that it must be able to register

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,39 @@
+import { OrgsRepository } from '../repositories/orgs-repository'
+import { hash } from 'bcryptjs'
+import type { Org } from '@prisma/client'
+
+interface RegisterUseCaseRequest {
+  nameOfPersonResponsible: string
+  email: string
+  password: string
+  cep: string
+  address: string
+  phone: string
+}
+
+interface RegisterUseCaseResponse {
+  org: Org
+}
+
 export class RegisterUseCase {
-  async execute() {
-    return 'Create'
+  constructor(private orgsRepository: OrgsRepository) {}
+  async execute({
+    address,
+    cep,
+    email,
+    nameOfPersonResponsible,
+    password,
+    phone,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+    const org = await this.orgsRepository.create({
+      nameOfPersonResponsible,
+      address,
+      cep,
+      email,
+      phone,
+      password_hash: await hash(password, 6),
+    })
+
+    return { org }
   }
 }
