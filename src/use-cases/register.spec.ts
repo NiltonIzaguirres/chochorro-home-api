8e98b15da9af24b2452e24d1f8861e89b68bdec5
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2024-06-27 13:18:08 -0300

    test: ensure that the password has been hashed

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it, beforeEach } from 'vitest'
 import { InMemoryOrgsRepository } from '../repositories/in-memory/in-memory-orgs-repository'
 import { RegisterUseCase } from './register'
 import { OrgAlreadyExistsError } from './erros/org-already-exists-error'
+import { compare } from 'bcryptjs'
 
 let orgsRepository: InMemoryOrgsRepository
 let sut: RegisterUseCase
@@ -51,5 +52,23 @@ describe('Register Use Case', async () => {
         phone: '1234567890',
       }),
     ).rejects.toBeInstanceOf(OrgAlreadyExistsError)
+  })
+
+  it('should hash org password upon registration', async () => {
+    const { org } = await sut.execute({
+      nameOfPersonResponsible: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'password123',
+      cep: '12345678',
+      address: '123 Main St',
+      phone: '1234567890',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      'password123',
+      org.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
   })
 })
