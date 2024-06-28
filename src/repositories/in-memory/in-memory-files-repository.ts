18bd0d2ddc85bbed7f78bfb7dbfd27fd9b2b4855
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2024-06-28 15:04:59 -0300

    feat: implement files repository

---
+++ src/repositories/in-memory/in-memory-files-repository.ts
@@ -0,0 +1,39 @@
+import { Prisma, File } from '@prisma/client'
+import { FilesRepository } from '../files-repository'
+import { randomUUID } from 'crypto'
+
+export class InMemoryFilesRepository implements FilesRepository {
+  public items: File[] = []
+
+  async create({ key, name, petId }: Prisma.FileUncheckedCreateInput) {
+    const file = {
+      id: randomUUID(),
+      key,
+      name,
+      petId,
+      createdAt: new Date(),
+    }
+
+    this.items.push(file)
+
+    return file
+  }
+
+  async delete(id: string) {
+    const index = this.items.findIndex((item) => item.id === id)
+
+    if (index >= 0) {
+      this.items.splice(index, 1)
+    }
+  }
+
+  async findByPetId(petId: string) {
+    const file = this.items.find((item) => item.petId === petId)
+
+    if (!file) {
+      return null
+    }
+
+    return file
+  }
+}
