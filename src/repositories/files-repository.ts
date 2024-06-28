d9be8716bd7ab297aaec3738e397f16091d721ef
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2024-06-28 14:46:04 -0300

    feat: add new methods to Files Repository

--- src/repositories/files-repository.ts
+++ src/repositories/files-repository.ts
@@ -1,1 +1,7 @@
-export interface FilesRepository {}
+import { Prisma, File } from '@prisma/client'
+
+export interface FilesRepository {
+  create(data: Prisma.FileUncheckedCreateInput): Promise<File>
+  delete(id: string): void
+  findByPetId(petId: string): Promise<File | null>
+}
