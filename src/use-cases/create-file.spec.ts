1510e404e4ae2fb0206c0ab3fbf160f12e85b2f0
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2024-06-28 19:51:27 -0300

    test: ensure that you can create pet image

---
+++ src/use-cases/create-file.spec.ts
@@ -0,0 +1,48 @@
+import { describe, expect, it, beforeEach, afterEach } from 'vitest'
+import { CreateFileUseCase } from './create-file'
+import { InMemoryFilesRepository } from '@/repositories/in-memory/in-memory-files-repository'
+import path from 'path'
+// import * as fs from 'fs-extra'
+import fs from 'fs'
+
+const TEST_DIR_FROM = path.join(process.cwd(), 'test', 'assets')
+const TEST_DIR_TO = path.join(process.cwd(), 'tmp')
+let filesRepository: InMemoryFilesRepository
+let sut: CreateFileUseCase
+
+describe('Create pet Use Case', async () => {
+  beforeEach(async () => {
+    filesRepository = new InMemoryFilesRepository()
+    sut = new CreateFileUseCase(filesRepository)
+    await fs.mkdirSync(TEST_DIR_TO, { recursive: true })
+  })
+
+  afterEach(async () => {
+    const files = fs.readdirSync(TEST_DIR_TO)
+
+    files.forEach((file) => {
+      const filePath = path.join(TEST_DIR_TO, file)
+      fs.unlinkSync(filePath)
+    })
+  })
+
+  it('should be able to create a image pet', async () => {
+    const { file } = await sut.execute({
+      fileData: await fs.createReadStream(
+        path.join(TEST_DIR_FROM, 'test-svg.svg'),
+      ),
+      mimeType: 'image/svg+xml',
+      fileName: 'test-svg.svg',
+      petId: 'org123',
+      uploadDir: TEST_DIR_TO,
+    })
+
+    expect(file).toEqual({
+      id: expect.any(String),
+      key: expect.stringContaining('test-svg'),
+      name: 'test-svg.svg',
+      petId: 'org123',
+      createdAt: expect.any(Date),
+    })
+  })
+})
