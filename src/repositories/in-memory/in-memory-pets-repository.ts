bf72487888409298f9594e8745dd9516558539e0
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2024-06-28 10:38:14 -0300

    feat: implement pets repository

---
+++ src/repositories/in-memory/in-memory-pets-repository.ts
@@ -0,0 +1,78 @@
+import { Pet, Prisma } from '@prisma/client'
+import { FindManyPetsByQueryParams, PetsRepository } from '../pets-repository'
+import { randomUUID } from 'crypto'
+
+export class InMemoryPetsRepository implements PetsRepository {
+  public items: Pet[] = []
+
+  async create(data: Prisma.PetUncheckedCreateInput) {
+    const pet: Pet = {
+      id: randomUUID(),
+      name: data.name,
+      about: data.about,
+      age: data.age,
+      energy: data.energy,
+      independence: data.independence,
+      environment: data.environment,
+      shape: data.shape,
+      createdAt: new Date(),
+      orgId: data.orgId,
+    }
+
+    this.items.push(pet)
+
+    return pet
+  }
+
+  async findById(id: string) {
+    const pet = this.items.find((item) => item.id === id)
+
+    if (!pet) {
+      return null
+    }
+
+    return pet
+  }
+
+  async findManyByQuery({
+    age,
+    energy,
+    environment,
+    independence,
+    shape,
+  }: FindManyPetsByQueryParams) {
+    let filteredPets = this.items
+
+    if (age) {
+      filteredPets = filteredPets.filter((pet) => pet.age === age)
+    }
+
+    if (energy) {
+      filteredPets = filteredPets.filter((pet) => pet.energy === energy)
+    }
+
+    if (environment) {
+      filteredPets = filteredPets.filter((pet) => pet.age === age)
+    }
+
+    if (independence) {
+      filteredPets = filteredPets.filter((pet) => pet.age === age)
+    }
+
+    if (shape) {
+      filteredPets = filteredPets.filter((pet) => pet.age === age)
+    }
+
+    return filteredPets
+  }
+
+  async save(pet: Pet) {
+    const petIndex = this.items.findIndex((item) => item.id === pet.id)
+
+    if (petIndex >= 0) {
+      this.items[petIndex] = pet
+    }
+
+    return pet
+  }
+}
