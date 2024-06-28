06fbda323f53db88dd1f7ea9c65ddbd4bf602d1f
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2024-06-28 10:38:56 -0300

    test: ensure that you can create pet

---
+++ src/use-cases/create-pet.spec.ts
@@ -0,0 +1,33 @@
+import { describe, expect, it, beforeEach } from 'vitest'
+import { CreatePetUseCase } from './create-pet'
+import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository'
+
+let petsRepository: InMemoryPetsRepository
+let sut: CreatePetUseCase
+
+describe('Create pet Use Case', async () => {
+  beforeEach(() => {
+    petsRepository = new InMemoryPetsRepository()
+    sut = new CreatePetUseCase(petsRepository)
+  })
+
+  it('should be able to create a pet', async () => {
+    const { pet } = await sut.execute({
+      name: 'John Doe',
+      about: 'A friendly and playful dog',
+      age: 3,
+      energy: 5,
+      independence: 'LOW',
+      environment: 'WIDE',
+      shape: 'BIG',
+      orgId: 'org123',
+    })
+
+    expect(pet).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        name: 'John Doe',
+      }),
+    )
+  })
+})
