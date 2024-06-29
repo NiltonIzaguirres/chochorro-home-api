8b34c42f473f6d3ad23b0f72d25514a0f3c59d27
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2024-06-29 17:59:00 -0300

    chore: add fastify static

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import fastify from 'fastify'
 import fastifyMultipart from '@fastify/multipart'
+import fastifyStatic from '@fastify/static'
+import path from 'path'
 
 export const app = fastify()
 
 app.register(fastifyMultipart)
+
+app.register(fastifyStatic, {
+  root: path.join(__dirname, '..', 'public'),
+  prefix: '/public/',
+})
