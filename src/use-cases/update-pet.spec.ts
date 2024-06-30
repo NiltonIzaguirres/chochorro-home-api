6f2adf2b13995187ba795e35ca25eaae0d13155a
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2024-06-30 16:32:21 -0300

    test: it should be able to update pet

---
+++ src/use-cases/update-pet.spec.ts
@@ -0,0 +1,66 @@
+import { describe, expect, it, beforeEach } from 'vitest'
+import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository'
+import { ResourceNotFoundError } from './erros/resource-not-found-error'
+import { UpdatePetUseCase } from './update-pet'
+
+let petsRepository: InMemoryPetsRepository
+let sut: UpdatePetUseCase
+
+describe('Create pet Use Case', async () => {
+  beforeEach(() => {
+    petsRepository = new InMemoryPetsRepository()
+    sut = new UpdatePetUseCase(petsRepository)
+  })
+
+  it('should be able to update a pet', async () => {
+    const createdPet = await petsRepository.create({
+      name: 'John Doe Smith',
+      about: 'A friendly',
+      age: 33,
+      energy: 1,
+      independence: 'MEDIUM',
+      environment: 'MEDIUM',
+      shape: 'SMALL',
+      orgId: 'org123',
+    })
+
+    const { pet } = await sut.execute({
+      id: createdPet.id,
+      name: 'John Doe',
+      about: 'A friendly and playful dog',
+      age: 3,
+      energy: 5,
+      independence: 'LOW',
+      environment: 'WIDE',
+      shape: 'BIG',
+      orgId: 'org123',
+      createdAt: createdPet.createdAt,
+    })
+
+    expect(pet).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        name: 'John Doe',
+        about: 'A friendly and playful dog',
+        energy: 5,
+      }),
+    )
+  })
+
+  it('should throw an error when trying to update pet a non-existent', async () => {
+    await expect(
+      sut.execute({
+        id: 'not-exist-id',
+        name: 'John Doe',
+        about: 'A friendly and playful dog',
+        age: 3,
+        energy: 5,
+        independence: 'LOW',
+        environment: 'WIDE',
+        shape: 'BIG',
+        orgId: 'org123',
+        createdAt: new Date(),
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
