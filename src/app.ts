e9fdc5f212061d92bc97b22659601a7804cf87d8
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2024-07-02 15:50:26 -0300

    feat: add orgs routes

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import fastify from 'fastify'
 import fastifyMultipart from '@fastify/multipart'
 import fastifyStatic from '@fastify/static'
 import path from 'path'
+import { orgsRoutes } from './http/controllers/orgs/routes'
 
 export const app = fastify()
 
 app.register(fastifyMultipart)
+
+app.register(orgsRoutes)
 
 app.register(fastifyStatic, {
   root: path.join(__dirname, '..', 'public'),
