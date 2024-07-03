cc9b8535e67bfbf43cc59964b4e693a371745630
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2024-07-03 19:10:11 -0300

    test: ensure that you can fetch pets without params

--- src/use-cases/fetch-pets-by-query.spec.ts
+++ src/use-cases/fetch-pets-by-query.spec.ts
@@ -10,6 +10,46 @@ describe('Fetch pets by query Use Case', async () => {
     sut = new FetchPetsByQueryUseCase(petsRepository)
   })
 
+  it('should fetch pets without params', async () => {
+    await petsRepository.create({
+      name: 'John Doe',
+      city: 'San Francisco',
+      type: 'dog',
+      about: 'A friendly and playful dog',
+      age: 3,
+      energy: 5,
+      independence: 'LOW',
+      environment: 'WIDE',
+      shape: 'BIG',
+      orgId: 'org123',
+    })
+
+    await petsRepository.create({
+      name: 'Get pet',
+      city: 'San Francisco',
+      type: 'dog',
+      about: 'This pet',
+      age: 20,
+      energy: 3,
+      independence: 'MEDIUM',
+      environment: 'MEDIUM',
+      shape: 'SMALL',
+      orgId: 'org123',
+    })
+
+    const { pets } = await sut.execute({
+      query: {},
+    })
+
+    expect(pets).toHaveLength(2)
+    expect(pets[0]).toEqual(
+      expect.objectContaining({
+        name: 'John Doe',
+        about: 'A friendly and playful dog',
+      }),
+    )
+  })
+
   it('should fetch pets with query params', async () => {
     await petsRepository.create({
       name: 'John Doe',
