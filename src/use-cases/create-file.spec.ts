024a29096145ae91a57577ea1d62f431b70f2d1f
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2024-07-11 17:31:00 -0300

    test: ensure that it is not possible to upload a file with a different org

--- src/use-cases/create-file.spec.ts
+++ src/use-cases/create-file.spec.ts
@@ -5,16 +5,21 @@ import path from 'path'
 import fs from 'node:fs'
 import { InvalidTypeError } from './erros/invalid-type-error'
 import { FailedToUploadFileError } from './erros/failed-to-upload-file-error'
+import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository'
+import { UnauthorizedError } from './erros/unauthorized-error'
 
 const TEST_DIR_FROM = path.join(process.cwd(), 'test', 'assets')
 const TEST_DIR_TO = path.join(process.cwd(), 'tmp')
+
 let filesRepository: InMemoryFilesRepository
+let petsRepository: InMemoryPetsRepository
 let sut: CreateFileUseCase
 
 describe('Create pet Use Case', async () => {
   beforeEach(async () => {
     filesRepository = new InMemoryFilesRepository()
-    sut = new CreateFileUseCase(filesRepository)
+    petsRepository = new InMemoryPetsRepository()
+    sut = new CreateFileUseCase(filesRepository, petsRepository)
     await fs.mkdirSync(TEST_DIR_TO, { recursive: true })
   })
 
@@ -28,21 +33,35 @@ describe('Create pet Use Case', async () => {
   })
 
   it('should be able to create a image pet', async () => {
+    const pet = await petsRepository.create({
+      name: 'John Doe',
+      city: 'San Francisco',
+      type: 'dog',
+      about: 'A friendly and playful dog',
+      age: 3,
+      energy: 5,
+      independence: 'LOW',
+      environment: 'WIDE',
+      shape: 'BIG',
+      orgId: 'org123',
+    })
+
     const { file } = await sut.execute({
       fileData: await fs.createReadStream(
         path.join(TEST_DIR_FROM, 'test-svg.svg'),
       ),
       mimeType: 'image/svg+xml',
       fileName: 'test-svg.svg',
-      petId: 'org123',
+      petId: pet.id,
       uploadDir: TEST_DIR_TO,
+      sub: pet.orgId,
     })
 
     expect(file).toEqual({
       id: expect.any(String),
       key: expect.stringContaining('test-svg'),
       name: 'test-svg.svg',
-      petId: 'org123',
+      petId: pet.id,
       createdAt: expect.any(Date),
       imageURL: expect.any(String),
     })
@@ -58,11 +77,25 @@ describe('Create pet Use Case', async () => {
         fileName: 'test-pdf.pdf',
         petId: 'org123',
         uploadDir: TEST_DIR_TO,
+        sub: 'org123',
       }),
     ).rejects.toBeInstanceOf(InvalidTypeError)
   })
 
   it('should return error if unable to save file', async () => {
+    const pet = await petsRepository.create({
+      name: 'John Doe',
+      city: 'San Francisco',
+      type: 'dog',
+      about: 'A friendly and playful dog',
+      age: 3,
+      energy: 5,
+      independence: 'LOW',
+      environment: 'WIDE',
+      shape: 'BIG',
+      orgId: 'org123',
+    })
+
     const fileData = await fs.createReadStream(
       path.join(TEST_DIR_FROM, 'test-svg.svg'),
     )
@@ -76,9 +109,38 @@ describe('Create pet Use Case', async () => {
         fileData,
         mimeType: 'image/svg+xml',
         fileName: 'test-svg.svg',
-        petId: 'org123',
+        petId: pet.id,
         uploadDir: TEST_DIR_TO,
+        sub: pet.orgId,
       }),
     ).rejects.toBeInstanceOf(FailedToUploadFileError)
+  })
+
+  it('should throw an error when trying to upload a file with wrong sub id', async () => {
+    const pet = await petsRepository.create({
+      name: 'John Doe',
+      city: 'San Francisco',
+      type: 'dog',
+      about: 'A friendly and playful dog',
+      age: 3,
+      energy: 5,
+      independence: 'LOW',
+      environment: 'WIDE',
+      shape: 'BIG',
+      orgId: 'org123',
+    })
+
+    await expect(
+      sut.execute({
+        fileData: await fs.createReadStream(
+          path.join(TEST_DIR_FROM, 'test-svg.svg'),
+        ),
+        mimeType: 'image/svg+xml',
+        fileName: 'test-svg.svg',
+        petId: pet.id,
+        uploadDir: TEST_DIR_TO,
+        sub: 'non-existent-id',
+      }),
+    ).rejects.toBeInstanceOf(UnauthorizedError)
   })
 })
